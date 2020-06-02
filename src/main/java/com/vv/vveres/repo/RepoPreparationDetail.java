6be6d8d1efdcbcfa4576c3871ce626566b071636
HungPham <hungrost@gmail.com>
2020-06-02 13:28:51 +0700

    edit table

--- src/main/java/com/vv/vveres/repo/RepoPreparationDetail.java
+++
@@ -1,9 +0,0 @@
-package com.vv.vveres.repo;
-
-import com.vv.vveres.table.TbPreparationDetail;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface RepoPreparationDetail  extends JpaRepository<TbPreparationDetail,Long> {
-}
